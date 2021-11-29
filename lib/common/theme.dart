7adcadbb55e3595cc71573ecf8f16df8b337a188
Reizky Yudha Lesmana <reizkyyudhalesmana@gmail.com>
2021-11-29 13:19:16 +0700

    main

---
+++ lib/common/theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color blueColor = const Color(0xff1761A0);
+Color yellowColor = const Color(0xffEBB700);
+Color orangeColor = const Color(0xffE78200);
+Color greyColor = const Color(0xff7E7E7E);
+Color blackColor = const Color(0xff000000);
+Color whiteColor = const Color(0xffFFFFFF);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  fontWeight: semiBold,
+  color: blackColor,
+);
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  fontWeight: medium,
+  color: greyColor,
+);
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  fontWeight: semiBold,
+  color: whiteColor,
+);
+
+FontWeight bold = FontWeight.w700;
+FontWeight semiBold = FontWeight.w600;
+FontWeight medium = FontWeight.w500;
+FontWeight light = FontWeight.w300;
